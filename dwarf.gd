7ce1f69e8b7f46dada5393421a92122ef8ecc17b
Sergej Gureev <sgureev@lanit.ru>
2019-04-21 11:50:42 +0300

    1

--- dwarf.gd
+++ dwarf.gd
@@ -4,8 +4,12 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 var direction
-var speed = 1000
+var x_speed = 1000
 # Called when the node enters the scene tree for the first time.
+
+enum DIRECTION {
+	N,NE,E,SE,S,SW,W,NW
+}
 func _ready():
 	pass # Replace with function body.
 
@@ -16,11 +20,11 @@ func _ready():
 func _input(event):
 	print(get_parent().get_node("dwarf").position.x)
 	if event.is_action_pressed("ui_right"):
-		move_and_slide(Vector2(speed, 0))
-		direction = "E"
+		move_and_slide(Vector2(x_speed, 0))
+		direction = DIRECTION.E
 	
 
 func _on_fat_area_entered(area):
 	print(area)
-	if direction == "E":
-				move_and_slide(Vector2(-speed, 0))
+	if direction == DIRECTION.E:
+				move_and_slide(Vector2(-x_speed, 0))
